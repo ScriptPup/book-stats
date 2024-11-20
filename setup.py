c63093e2197de0e5064c2825ac9cd3130f3dcf69
scriptpup <ethan_hammond@msn.com>
2024-11-19 19:19:58 -0500

    Set version to 1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
    name='book-stats',
-   version='0.1',
+   version='1.0',
    packages=['beautifulsoup4','markdown','mplcyberpunk','matplotlib','wordcloud','setuptools','filedialpy','TkAgg'],
    license='GNU',
    packages=['bookstats'],
