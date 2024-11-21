3db6556ef3695c8de078f241c8dfae568aa16e29
scriptpup <ethan_hammond@msn.com>
2024-11-20 20:24:32 -0500

    Add PyQt6 requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
    name='book-stats',
    version='1.0',
-   packages=['beautifulsoup4','markdown','mplcyberpunk','matplotlib','wordcloud','setuptools','filedialpy'],
+   packages=['beautifulsoup4','markdown','mplcyberpunk','matplotlib','wordcloud','setuptools','filedialpy','PyQt6'],
    license='GNU',
    packages=['bookstats'],
    long_description=open('README.txt').read(),
